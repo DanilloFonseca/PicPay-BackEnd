17427c978e91ecccf4271f416504b577894f7228
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-03-24 16:40:40 -0300

    feat: create UserService

---
+++ src/main/java/com/fonseca/DesafioBackEnd/Service/UserService.java
@@ -0,0 +1,36 @@
+package com.fonseca.DesafioBackEnd.Service;
+
+import com.fonseca.DesafioBackEnd.domain.user.User;
+import com.fonseca.DesafioBackEnd.domain.user.UserType;
+import com.fonseca.DesafioBackEnd.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validationUserTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getType() != UserType.COMMON){
+            throw new Exception("User type not allowed to do a transfer");
+        }
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("User has not enough money to complete the transaction");
+        }
+
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(
+                () -> new Exception("User not found")
+                );
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+}
