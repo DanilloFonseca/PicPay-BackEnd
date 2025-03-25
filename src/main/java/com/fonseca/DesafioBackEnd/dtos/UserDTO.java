7440dc011a8100abd57a9125af160f1feda4e6d2
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-03-25 19:06:16 -0300

    feat: create UserDTO

---
+++ src/main/java/com/fonseca/DesafioBackEnd/dtos/UserDTO.java
@@ -0,0 +1,13 @@
+package com.fonseca.DesafioBackEnd.dtos;
+
+import com.fonseca.DesafioBackEnd.domain.user.User;
+import com.fonseca.DesafioBackEnd.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, String email, String password, UserType userType, BigDecimal balance) {
+
+    public UserDTO(User data){
+        this(data.getFirstName(), data.getLastName(), data.getDocument(), data.getEmail(), data.getPassword(), data.getType(), data.getBalance());
+    }
+}
