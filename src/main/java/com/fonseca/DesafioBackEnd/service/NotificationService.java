f2d7b38ae11b5a978977e44dfb3483a59ad498df
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-03-31 17:59:47 -0300

    refactor: moved to dir service

--- src/main/java/com/fonseca/DesafioBackEnd/service/NotificationService.java
+++ src/main/java/com/fonseca/DesafioBackEnd/service/NotificationService.java
@@ -1,12 +1,14 @@
-package com.fonseca.DesafioBackEnd.Service;
+package com.fonseca.DesafioBackEnd.service;
 
 import com.fonseca.DesafioBackEnd.domain.user.User;
 import com.fonseca.DesafioBackEnd.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class NotificationService {
 
     @Autowired
