69351cdbdf8d8dd73aac1a0b54d17f24514b2fd3
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-03-31 17:53:18 -0300

    feat: create transactionDTO constructor

--- src/main/java/com/fonseca/DesafioBackEnd/dtos/TransactionDTO.java
+++ src/main/java/com/fonseca/DesafioBackEnd/dtos/TransactionDTO.java
@@ -1,6 +1,11 @@
 package com.fonseca.DesafioBackEnd.dtos;
 
+import com.fonseca.DesafioBackEnd.domain.transaction.Transaction;
+
 import java.math.BigDecimal;
 
 public record TransactionDTO(BigDecimal amount, Long sender_id, Long receiver_id) {
+    public TransactionDTO(Transaction data){
+        this(data.getAmount(), data.getSender().getId(), data.getReceiver().getId());
+    }
 }
