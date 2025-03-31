5a0f5efda6866994568d53e584d6961674ec2015
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-03-31 18:29:57 -0300

    refactor: add getId in the UserDTO constructor

--- src/main/java/com/fonseca/DesafioBackEnd/dtos/UserDTO.java
+++ src/main/java/com/fonseca/DesafioBackEnd/dtos/UserDTO.java
@@ -5,9 +5,9 @@ import com.fonseca.DesafioBackEnd.domain.user.UserType;
 
 import java.math.BigDecimal;
 
-public record UserDTO(String firstName, String lastName, String document, String email, String password, UserType userType, BigDecimal balance) {
+public record UserDTO(String firstName, String lastName, String document, String email, String password, UserType userType, BigDecimal balance, Long user_id) {
 
     public UserDTO(User data){
-        this(data.getFirstName(), data.getLastName(), data.getDocument(), data.getEmail(), data.getPassword(), data.getType(), data.getBalance());
+        this(data.getFirstName(), data.getLastName(), data.getDocument(), data.getEmail(), data.getPassword(), data.getType(), data.getBalance(), data.getId());
     }
 }
