bbc91d4787db50947fe16b7a1e074565cb3d3eb7
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-04-01 16:03:17 -0300

    refactor: improved code editing and structure

--- src/main/java/com/fonseca/DesafioBackEnd/domain/transaction/Transaction.java
+++ src/main/java/com/fonseca/DesafioBackEnd/domain/transaction/Transaction.java
@@ -18,12 +18,16 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private BigDecimal amount;
+
     @ManyToOne
     @JoinColumn(name="sender_id")
     private User sender;
+
     @ManyToOne
     @JoinColumn(name="receiver_id")
     private User receiver;
+
     private LocalDateTime timestamp;
 }
